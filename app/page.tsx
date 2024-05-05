7afc34a799f47115cd7569789c2a32b5ff776d99
kriptoe <56439317+kriptoe@users.noreply.github.com>
2024-05-05 22:51:24 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -5,32 +5,24 @@ import { NEXT_PUBLIC_URL } from './config';
 const frameMetadata = getFrameMetadata({
   buttons: [
     {
-      label: 'Story time',
-    },
-    {
-      action: 'tx',
-      label: 'Send Base Sepolia',
-      target: `${NEXT_PUBLIC_URL}/api/tx`,
-      postUrl: `${NEXT_PUBLIC_URL}/api/tx-success`,
+      action: 'link',
+      label: 'Link to Google',
+      target: 'https://www.google.com',
     },
   ],
   image: {
-    src: `${NEXT_PUBLIC_URL}/Elisa.jpg`,
+    src: `${NEXT_PUBLIC_URL}/park-3.png`,
     aspectRatio: '1:1',
   },
-  input: {
-    text: 'Tell me a story',
-  },
-  postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
 });
 
 export const metadata: Metadata = {
-  title: 'Hypersolid',
-  description: 'Hypersolid',
+  title: 'Frame Test',
+  description: 'LFG',
   openGraph: {
-    title: 'Hypersolid',
-    description: 'Degen Lottery',
-    images: [`${NEXT_PUBLIC_URL}/Elisa.png`],
+    title: 'Frame Test',
+    description: 'LFG',
+    images: [`${NEXT_PUBLIC_URL}/Elisa.jpg`],
   },
   other: {
     ...frameMetadata,
@@ -40,7 +32,7 @@ export const metadata: Metadata = {
 export default function Page() {
   return (
     <>
-      <h1>Hypersolid NFT</h1>
+      <h1>Hypersolid</h1>
     </>
   );
 }
