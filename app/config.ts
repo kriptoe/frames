bd1524b9a93fc192ddb0a5283a2b979a9c90b6eb
kriptoe <56439317+kriptoe@users.noreply.github.com>
2024-05-18 20:30:00 +0800

    Update config.ts

--- app/config.ts
+++ app/config.ts
@@ -1,4 +1,4 @@
 // use NODE_ENV to not have to change config based on where it's deployed
 export const NEXT_PUBLIC_URL =
-  process.env.NODE_ENV == 'development' ? 'http://localhost:3000' : 'https://frames-phi.vercel.app/';
-export const BUY_MY_COFFEE_CONTRACT_ADDR = '0xcD3D5E4E498BAb2e0832257569c3Fd4AE439dD6f';
+  process.env.NODE_ENV == 'development' ? 'http://localhost:3000' : 'https://frames-gamma.vercel.app/';
+export const BUY_MY_COFFEE_CONTRACT_ADDR = '0xE90Eee57653633E7558838b98F543079649c9C2F';
